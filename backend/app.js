1b5288041d43b855ad22b0d8835123669f78422b
DispicableLee <lee.robert053@gmail.com>
2023-11-25 12:27:19 -0500

    errors

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var logger = require('morgan');
 const cors = require('cors');
 const { isProduction } = require('./config/keys');
 const csurf = require('csurf');
+const debug = require('debug');
 
 
 const csrfRouter = require('./routes/api/csrf');
@@ -51,5 +52,29 @@ app.use('/api/tracks', tracksRouter)
 app.use('/api/playlists', playlistsRouter)
 app.use('/api/users', usersRouter);
 app.use('/api/csrf', csrfRouter);
+
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 
 module.exports = app;
