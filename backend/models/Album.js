3f92247b44b8d0fbdc2145fb6706510a06e547b4
DispicableLee <lee.robert053@gmail.com>
2023-12-26 17:11:41 -0500

    got the album model kinda-down

--- backend/models/Album.js
+++ backend/models/Album.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AlbumSchema = new Schema({
-
+    uploader:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tracks: Array,
+    imageUrl: String,
+    
 })
 
 module.exports = mongoose.model('Album', AlbumSchema)
