d3051cbd6f8f23eaf719c6cb0b1eeb681050549e
DispicableLee <lee.robert053@gmail.com>
2024-08-15 13:04:45 -0400

    dunno what changes i made, just making a commit

--- frontend/src/components/ProfileShow/ProfileShow.js
+++ frontend/src/components/ProfileShow/ProfileShow.js
@@ -19,6 +19,26 @@ export default function ProfileShow(){
     },[dispatch])
 
 
+    const flexWrappedUserStats = function(){
+        return (
+            <div className="user-stat-box">
+                <div className="user-stat-card">
+                    <h1>{shownUser?.albums.length}</h1>
+                    <h4>Albums</h4>
+                </div>
+                <div className="user-stat-card">
+                    <h1>{shownUser?.albums.length}</h1>
+                    <h4>Albums</h4>
+                </div>
+                <div className="user-stat-card">
+                    <h1>{shownUser?.tracks.length}</h1>
+                    <h4>Tracks</h4>
+                </div>
+            </div>
+        )
+    }
+
+
 
     return (
         <div id="profile-show-main">
@@ -35,21 +55,52 @@ export default function ProfileShow(){
                 <div className="user-stats-main">
 
                     <div className="user-stats">
-                        <div className="user-content-stats">
-                            <div className="user-stat-box">
+                        <div className="user-stat-box">
+                            <div className="user-stat-card">
                                 <h1>{shownUser?.albums.length}</h1>
                                 <h4>Albums</h4>
                             </div>
-                            <div className="user-stat-box">
+                            <div className="user-stat-card">
                                 <h1>{shownUser?.playlists.length}</h1>
                                 <h4>Playlists</h4>
                             </div>
-                            <div className="user-stat-box">
+                            <div className="user-stat-card">
                                 <h1>{shownUser?.tracks.length}</h1>
                                 <h4>Tracks</h4>
                             </div>
+                        </div>  
+
+
+                        <div className="user-info">
+                            <div className="username">
+                                <h1>
+                                    {shownUser?.username}
+                                </h1>
+                            </div>
+                            <div className="links">
+                                Instagram
+                                <br/>
+                                LinkedIn
+                                <br/>
+                                GitHub
+                                <br/>
+                                
+                            </div>  
                         </div>
+
+
+
+
                     </div>
+
+                    <section className="right-nav">
+
+                    </section>
+
+
+
+
+
                 </div>
             </div>
         </div>
