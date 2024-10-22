f86be31e681af98cf8c4563c5b4a4386d858abc1
DispicableLee <lee.robert053@gmail.com>
2024-10-22 14:03:57 -0400

    csrf

--- backend/routes/api/csrf.js
+++ backend/routes/api/csrf.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const { isProduction } = require('../../config/keys');
 
-if (!isProduction) {
+// if (!isProduction) {
   // In development, allow developers to access the CSRF token to test the
   // server endpoints in Postman.
   router.get("/restore", (req, res) => {
@@ -14,6 +14,6 @@ if (!isProduction) {
       'CSRF-Token': csrfToken
     });
   });
-}
+// }
 
 module.exports = router;
